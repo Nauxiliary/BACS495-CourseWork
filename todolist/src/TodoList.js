e4272757f057b15b0ff2e174d190015416441cf5
Daniel Edwards <edwa3545@bears.unco.edu>
2022-02-08 01:53:56 -0700

    finish todolist

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,18 +1,41 @@
 import React from 'react';
 
-function TodoList() {
-    function addTodoItem(e) {
-        console.log('hi');
+class TodoList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state={todolist:[]};
+
+        this.addTodoItem = this.addTodoItem.bind(this);
+        this.removeTodoItem = this.removeTodoItem.bind(this);
+    }
+    
+    addTodoItem() {
+        var todolist = [...this.state.todolist]
+        todolist.push(this.newItem.value);
+        this.setState({todolist});
     }
 
-    return (
-    <div className="TodoList">
-    <form onSubmit={addTodoItem}>
-        <input type="text">
-        </input>
-        <button type="submit">Add</button>
-    </form>
-    </div>
-    );
+    removeTodoItem(item) {
+        const list = this.state.todolist;
+
+        list.splice(item, 1);
+        this.setState({ list });
+    }
+
+    render() {
+        return(
+                <div>
+                    <input type="text" ref={(item) => {this.newItem = item}}/>
+                    <button className="TodolistButton" onClick={this.addTodoItem}>Add</button>
+                    <ul className="Todolist">
+                        {this.state.todolist.map((x,y) => {
+                            return <li key={y}>{x}
+                            <button className="TodolistButton" onClick={this.removeTodoItem}>Remove</button>
+                            </li>})}
+                    </ul>
+                </div>
+        )
+    }
 }
 export default TodoList;
